320c4ffb7865ff70ce8c3a15426da6a4e01cfb23
technicalanxiety <jason.rinehart@technicalanxiety.com>
2025-11-26 23:25:19 -0600

    removed many redundant templates

--- infrastructure/arm-templates/resources/LogicApps/test.sh
+++
@@ -1,9 +0,0 @@
-az login --username devuser@msp.com 
-
-az account set --subscription subscription-dev-001
-az account list
-
-az group create --name rgTestDave --location "eastus"
-az group deployment create --resource-group rgTestDave --template-file LogicApps.json
-
-az group delete --name rgTestDave
